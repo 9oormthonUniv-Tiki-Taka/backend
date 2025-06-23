56042cb2ad72d88516f75f348d0fd96a0789a0e2
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-23 21:26:48 +0900

    feat: QuestionController 기본 구현

--- api/src/main/java/com/tikitaka/api/controller/LectureController.java
+++ api/src/main/java/com/tikitaka/api/controller/LectureController.java
@@ -1,4 +1,19 @@
 package com.tikitaka.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/lectures")
 public class LectureController {
+
+    @GetMapping
+    public ResponseEntity<?> getLectureList() {
+        return ResponseEntity.ok("수업 목록 조회");
+    }
+
+    @GetMapping("/{lectureId}/live/questions")
+    public ResponseEntity<?> getLiveQuestions(@PathVariable Long lectureId) {
+        return ResponseEntity.ok("실시간 대화 내역 조회");
+    }
 }
--- api/src/main/java/com/tikitaka/api/controller/SocketController.java
+++ api/src/main/java/com/tikitaka/api/controller/SocketController.java
@@ -1,4 +1,10 @@
 package com.tikitaka.api.controller;
 
-public class SocketController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Controller
+@RequestMapping("/api/lectures/{lectureId}/live")
+public class SocketController extends TextWebSocketHandler {
 }
