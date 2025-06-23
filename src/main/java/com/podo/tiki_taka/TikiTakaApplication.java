2dedd5f1352713711bedc3ad1b4a526409cb1f6b
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-23 19:58:58 +0900

    first commit

---
+++ src/main/java/com/podo/tiki_taka/TikiTakaApplication.java
@@ -0,0 +1,13 @@
+package com.podo.tiki_taka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikiTakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikiTakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/podo/tiki_taka/TikiTakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podo.tiki_taka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikiTakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
