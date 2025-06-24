bb36e680feac9e7c5718c03ad8dc58135f5fb112
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-24 18:55:07 +0900

    feat: swagger 기본 구현

--- api/src/main/java/com/tikitaka/api/config/SwaggerConfig.java
+++ api/src/main/java/com/tikitaka/api/config/SwaggerConfig.java
@@ -1,5 +1,30 @@
 package com.tikitaka.api.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tiki-Taka API 문서")
+                        .description("API 명세서")
+                        .version("v1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
 }
+
