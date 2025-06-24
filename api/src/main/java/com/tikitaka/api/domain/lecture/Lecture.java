85a122f083b47f22e2c881eabdf879a4a7c36651
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-24 10:21:46 +0900

    lecture 수정

--- api/src/main/java/com/tikitaka/api/domain/lecture/Lecture.java
+++ api/src/main/java/com/tikitaka/api/domain/lecture/Lecture.java
@@ -22,8 +22,8 @@ public class Lecture extends BaseTimeEntity {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "professor_id")
-    private User professor;
+    @JoinColumn(name = "user_id")
+    private User user;
 
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
