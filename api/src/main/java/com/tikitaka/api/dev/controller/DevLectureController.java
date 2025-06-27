a731f70a6c2631e3dfad89ce69eec0500e399362
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-28 01:20:59 +0900

    feat: lecture 전체 조회

--- api/src/main/java/com/tikitaka/api/dev/controller/DevLectureController.java
+++ api/src/main/java/com/tikitaka/api/dev/controller/DevLectureController.java
@@ -1,12 +1,15 @@
 package com.tikitaka.api.dev.controller;
 
 import com.tikitaka.api.dev.dto.LectureRequest;
+import com.tikitaka.api.dev.dto.LectureResponse;
 import com.tikitaka.api.dev.service.lecture.DevLectureService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Dev API", description = "개발용 API 모음")
 @RestController
@@ -20,6 +23,12 @@ public class DevLectureController {
     public ResponseEntity<Void> registerLecture(@RequestBody LectureRequest request) {
         lectureService.registerLecture(request);
         return ResponseEntity.ok().build();
+    }
+    @Operation(summary = "전체 강의 조회", description = "모든 강의 목록을 조회합니다.")
+    @GetMapping
+    public ResponseEntity<List<LectureResponse>> getAllLectures() {
+        List<LectureResponse> lectures = lectureService.getAllLectures();
+        return ResponseEntity.ok(lectures);
     }
 }
 
