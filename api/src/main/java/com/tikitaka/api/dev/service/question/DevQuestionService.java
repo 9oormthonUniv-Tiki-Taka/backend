5e91a3e530f099e531e69b5350e80e11f5054fe2
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-06-28 01:16:05 +0900

    feat: questions 전체 조회

--- api/src/main/java/com/tikitaka/api/dev/service/question/DevQuestionService.java
+++ api/src/main/java/com/tikitaka/api/dev/service/question/DevQuestionService.java
@@ -1,7 +1,11 @@
 package com.tikitaka.api.dev.service.question;
 
 import com.tikitaka.api.dev.dto.QuestionRequest;
+import com.tikitaka.api.dev.dto.QuestionResponse;
+
+import java.util.List;
 
 public interface DevQuestionService {
     void registerQuestion(QuestionRequest request);
+    List<QuestionResponse> getAllQuestions();
 }
